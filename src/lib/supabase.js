3cd378512b3ad6c1a0b7ccaec853daa077173f96
Jose <aracenajose58@gmail.com>
2026-03-30 13:16:30 -0400

    fix: Evitar pantallazo en blanco al faltar variables de entorno en Vercel

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,9 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || '';
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || '';
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+// Si las variables de entorno no existen, enviaremos un 'null' para no tirar la página entera en blanco
+export const supabase = supabaseUrl && supabaseAnonKey 
+  ? createClient(supabaseUrl, supabaseAnonKey) 
+  : null;
